e7431b63770cae0c1f639a9bad0bfb8d8ee16807
Vsevolod Chernetskyi <vchernetskyi993@gmail.com>
2025-11-01 09:31:15 +0200

    ED-168 - listing applications

--- linux/installer/configs/switcher/app-switcher.sh
+++ linux/installer/configs/switcher/app-switcher.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-# Dependencies:
-# * fzf
-# * window-calls@domandoman.xyz
-
 function main() {
-    list-windows |
-        fzf --accept-nth=1 -d ',' --with-nth=2 |
+    list-all |
+        fzf --accept-nth=1 -d ',' --with-nth=2 --tiebreak=index |
         focus-window
+}
+
+function list-all() {
+    list-windows
+    list-applications
+}
+
+function list-applications() {
+    local data_home="${XDG_DATA_HOME:-$HOME/.local/share}"
+    local data_dirs="${XDG_DATA_DIRS:-/usr/local/share:/usr/share}"
+    local all_dirs="$data_home:$data_dirs"
+
+    # Use find to list all .desktop files, remove suffix & duplicates
+    echo "$all_dirs" | tr ':' '\n' | while read -r dir; do
+        find "$dir/applications" -type f -name '*.desktop' \
+            -exec basename {} .desktop \; 2>/dev/null
+    done | sort -u | while read -r app; do
+        echo "app:${app},app: ${app}"
+    done
 }
 
 function list-windows() {
@@ -16,21 +31,30 @@ function list-windows() {
         --object-path /org/gnome/Shell/Extensions/Windows \
         --method org.gnome.Shell.Extensions.Windows.List |
         sed "s/^('\(.*\)',)$/\1/" |
-        jq -r '.[] | "\(.id),window: [\(.wm_class)] \(.title)"'
+        jq -r '.[] | select(.title != "app-switcher") | "win:\(.id),win: [\(.wm_class)] \(.title)"'
 }
 
 function focus-window() {
-    local winid
-    read -r winid
+    local id
+    read -r id
 
-    if [[ -z "$winid" ]]; then
+    if [[ -z "$id" ]]; then
         return
     fi
 
-    gdbus call --session \
-        --dest org.gnome.Shell \
-        --object-path /org/gnome/Shell/Extensions/Windows \
-        --method org.gnome.Shell.Extensions.Windows.Activate "$winid"
+    case "$id" in
+    win:*)
+        winid="${id#win:}"
+        gdbus call --session \
+            --dest org.gnome.Shell \
+            --object-path /org/gnome/Shell/Extensions/Windows \
+            --method org.gnome.Shell.Extensions.Windows.Activate "$winid"
+        ;;
+    app:*)
+        appid="${id#app:}"
+        gtk-launch "$appid"
+        ;;
+    esac
 }
 
 [[ "${BASH_SOURCE[0]}" != "${0}" ]] || main $@
