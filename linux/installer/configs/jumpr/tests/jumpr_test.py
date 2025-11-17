e6f7219cc8bd68638a66b82ca15d69a4e6e5b3d7
Vsevolod Chernetskyi <vchernetskyi993@gmail.com>
2025-11-17 20:11:08 +0200

    ED-173 [jumpr] - python - switch window wip

--- linux/installer/configs/jumpr/tests/jumpr_test.py
+++ linux/installer/configs/jumpr/tests/jumpr_test.py
@@ -1,4 +1,23 @@
+import os
+from pathlib import Path
+import subprocess
 
-def test_switch_windows():
-    assert False
+from pytest import MonkeyPatch
 
+
+def test_switch_windows(monkeypatch: MonkeyPatch) -> None:
+    # given
+    mocks_path = Path("./bin/").resolve()
+    monkeypatch.setenv("PATH", f"{mocks_path}:{os.environ['PATH']}")
+    monkeypatch.setenv("FZF_DEFAULT_OPTS", "--query 'My Window' --exact -1 -0")
+    # mock list applications
+
+    # when
+    result = subprocess.run(["../jumpr.sh"], capture_output=True, text=True)
+
+    # then
+    assert result.returncode == 0
+    assert result.stderr == ""
+    assert result.stdout == ""
+    # assert 2 gdbus calls
+    # assert window activate called with correct arguments
