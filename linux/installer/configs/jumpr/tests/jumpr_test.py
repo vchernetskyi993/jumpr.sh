1e63e799b8606236bfa28d6c72b4db1b9989fe1d
Vsevolod Chernetskyi <vchernetskyi993@gmail.com>
2025-11-20 19:45:51 +0200

    ED-173 [jumpr] - mock as a class

--- linux/installer/configs/jumpr/tests/jumpr_test.py
+++ linux/installer/configs/jumpr/tests/jumpr_test.py
@@ -1,22 +1,21 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 import os
 from pathlib import Path
+import shutil
 import subprocess
 
 import simple_parsing
 from pytest import MonkeyPatch, FixtureRequest
+from simple_parsing.utils import DataclassT
 
 
 def test_switch_windows(
     monkeypatch: MonkeyPatch, tmp_path: Path, request: FixtureRequest
 ) -> None:
     # given
-    mocks_path = Path("./bin/").resolve()
-    monkeypatch.setenv("PATH", f"{mocks_path}:{os.environ['PATH']}")
+    bin_factory = BinFactory(tmp_path, monkeypatch, request)
+    gdbus = bin_factory.mock("gdbus")
     monkeypatch.setenv("FZF_DEFAULT_OPTS", "--query 'My Window' --exact -1 -0")
-    out_path = tmp_path / "out"
-    monkeypatch.setenv("OUT_DIR", str(out_path))
-    monkeypatch.setenv("MOCKS_DIR", f"./mocks/{request.node.name}")
     # mock applications
     # mock $HOME
 
@@ -28,24 +27,13 @@ def test_switch_windows(
     assert result.stderr == ""
     assert result.stdout == ""
 
-    with open(out_path / "gdbus_args", "r") as args_file:
-        lines = args_file.readlines()
-        assert len(lines) == 2
-        print(lines[1].split()[2:])
-        activation_args = simple_parsing.parse_known_args(
-            GDBusArgs, args=lines[1].split()
-        )
-        assert activation_args == (
-            GDBusArgs(
-                command="call",
-                session=True,
-                dest="org.gnome.Shell",
-                object_path="/org/gnome/Shell/Extensions/Windows",
-                method="org.gnome.Shell.Extensions.Windows.Activate",
-                window_id=12345,
-            ),
-            [],
-        )
+    args = gdbus.received_args()
+    assert len(args) == 2
+    assert _parse(GDBusArgs, args[1]) == _activate_window(12345)
+
+
+def _parse(type: type[DataclassT], args: str) -> DataclassT:
+    return simple_parsing.parse(type, args=args)
 
 
 @dataclass
@@ -56,3 +44,41 @@ class GDBusArgs:
     dest: str = ""
     object_path: str = simple_parsing.field(default="", alias="--object-path")
     method: str = ""
+
+
+def _activate_window(window_id: int) -> GDBusArgs:
+    return GDBusArgs(
+        command="call",
+        session=True,
+        dest="org.gnome.Shell",
+        object_path="/org/gnome/Shell/Extensions/Windows",
+        method="org.gnome.Shell.Extensions.Windows.Activate",
+        window_id=window_id,
+    )
+
+
+@dataclass
+class Mock:
+    out_path: Path
+    executable: str
+
+    def received_args(self) -> list[str]:
+        with open(self.out_path / f"{self.executable}_args", "r") as args_file:
+            return args_file.readlines()
+
+
+@dataclass
+class BinFactory:
+    tmp_path: Path
+    monkeypatch: MonkeyPatch
+    request: FixtureRequest
+
+    def mock(self, executable: str) -> Mock:
+        bin_dir = self.tmp_path / "bin"
+        bin_dir.mkdir(exist_ok=True)
+        _ = shutil.copy2("./bin/stub", bin_dir / executable)
+        self.monkeypatch.setenv("PATH", f"{bin_dir}:{os.environ['PATH']}")
+        out_path = self.tmp_path / "out"
+        self.monkeypatch.setenv("OUT_DIR", str(out_path))
+        self.monkeypatch.setenv("MOCKS_DIR", f"./mocks/{self.request.node.name}")
+        return Mock(out_path, executable)
