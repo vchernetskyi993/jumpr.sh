64fafc0a9eb4c8e44d9b13b25890fb242dffab7d
Vsevolod Chernetskyi <vchernetskyi993@gmail.com>
2025-11-22 17:04:44 +0200

    ED-173 [jumpr] - missing data dir

--- linux/installer/configs/jumpr/tests/list_applications_test.py
+++ linux/installer/configs/jumpr/tests/list_applications_test.py
@@ -2,6 +2,14 @@ import subprocess
 
 
 def test_empty_data_dirs() -> None:
+    _list_applications()
+
+
+def test_missing_data_dirs() -> None:
+    _list_applications()
+
+
+def _list_applications() -> None:
     # when
     result = subprocess.run(
         ["/bin/bash", "-c", "source ../jumpr.sh && list-applications"],
